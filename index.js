b730ef8506c3f1ec435ad6d909398a4cee890f4d
gauravo7 <gaurav@o7services.com>
2025-04-23 10:52:11 +0530

    pm2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World with pm2')
 })
 
 app.listen(3000, '0.0.0.0', function(){
